3f5ff743200a3d8e35a851a23f1eeec9124da726
AlejoSuaterna <pablo.gonzalez01@correo.usa.edu.co>
2022-09-14 14:21:13 -0500

    se agrega bootstrap y navBar

--- front-panini/src/components/NavBar/NavBar.js
+++ front-panini/src/components/NavBar/NavBar.js
@@ -1,6 +1,46 @@
 
-export default function NavBar(){
-    return(
-        <div>aqui va la NavBar</div>
-    )
+export default function NavBar() {
+  return (
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">My Panini</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">My Panini</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Album</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Colección
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                  <li><a class="dropdown-item" href="#">Todo</a></li>
+                  <li><a class="dropdown-item" href="#">Repetidas</a></li>
+                  <li>
+                    <hr class="dropdown-divider"/>
+                  </li>
+                  <li><a class="dropdown-item" href="#">...</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                <button class="btn btn-success" type="submit">Buscar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
